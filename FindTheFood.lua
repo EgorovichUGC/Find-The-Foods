adbae80d50dca83f31560fc3801feb274014ee1c
EgorovichUGC <egorsyulgin2016@gmail.com>
2024-07-05 10:07:44 +0300

    Update FindTheFood.lua

--- FindTheFood.lua
+++ FindTheFood.lua
@@ -240,7 +240,7 @@ ugc:Toggle("Taco (Teleport to Door)", false, function(bool)
           end
         end)
 
-         ugc:Toggle("Anti AFK", false, function(bool)
+                 ugc:Toggle("Anti Afk", false, function(bool)
 
           loopActive = bool
 
@@ -276,7 +276,7 @@ Title.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
 Title.BorderSizePixel = 0
 Title.Size = UDim2.new(0, 295, 0, 16)
 Title.Font = Enum.Font.GothamBold
-Title.Text = "Anti AFK"
+Title.Text = "Anti Afk"
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
 Title.TextScaled = true
 Title.TextSize = 12.000
@@ -316,14 +316,6 @@ Activate.MouseButton1Down:connect(function()
 	end)
 end)
 
-				
-              wait(0)
-              if loopActive == false then
-                break
-              end
-            end
-          end
-        end)
 
 UICorner.Parent = Activate
 
@@ -349,7 +341,12 @@ local function NERMBF_fake_script()
 	end)
 end
 coroutine.wrap(NERMBF_fake_script)()
-
-
-
+				
+              wait(0)
+              if loopActive == false then
+                break
+              end
+            end
+          end
+        end)
 -- BY Egorovich
